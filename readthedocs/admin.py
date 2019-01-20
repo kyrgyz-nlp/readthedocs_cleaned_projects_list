31bf17d4bac0340712015fae720ff78ae1237191
muratjumashev <jumasheff@gmail.com>
2019-01-21 00:48:02 +0600

    Modify admin panel

--- readthedocs/admin.py
+++ readthedocs/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from readthedocs.models import Project
 
 class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'canonical_url')
     search_fields = ['description']
     list_filter = ['programming_language', 'documentation_type']
 
