c8e3b0ec46ee7d869ea39d189346889d0803785c
muratjumashev <jumasheff@gmail.com>
2019-01-20 21:58:24 +0600

    Use postgres

--- config/settings.py
+++ config/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'NAME': 'readthedocs',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
